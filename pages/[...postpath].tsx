f051814e08120843133811b07d2196ec2f2cf4bf
5box00a <138788564+5box00a@users.noreply.github.com>
2023-07-06 20:24:36 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://sofiabahan.000webhostapp.com/graphql` + encodeURI(path as string)
+					`https://sofiabahan.000webhostapp.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
